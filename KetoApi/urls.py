67ca5cb6353d66d233ff69d1e911f066f5d5543f
zadokchrist <ngobizadokchrist@gmail.com>
2023-02-01 12:14:40 +0300

    configuring swagger documentation

--- KetoApi/urls.py
+++ KetoApi/urls.py
@@ -37,6 +37,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('authentication.urls')),
     path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
